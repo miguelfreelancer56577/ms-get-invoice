3e28b1fdbede60c1b23647915239d155580d6bcd
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2019-09-04 10:50:15 -0500

    Implemented ribbon along with eureka to call another microservice

--- src/main/java/com/github/mangelt/ms/get/invoice/App.java
+++ src/main/java/com/github/mangelt/ms/get/invoice/App.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+import feign.Request;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
-@EnableFeignClients("com.github.mangelt.ms.get.invoice.service")
+@EnableFeignClients
 @EnableDiscoveryClient
 @EnableWebFlux
 @Slf4j
@@ -24,4 +22,10 @@ public class App
 		log.info("Starting ms-get-invoice spring boot application.");
 		SpringApplication.run(App.class, args);
 	}
+
+	@Bean
+	public Request.Options timeoutConfiguration(){
+		return new Request.Options(5000, 30000);
+	}
+
 }
--- src/main/java/com/github/mangelt/ms/get/invoice/service/ComprobantesProxy.java
+++ src/main/java/com/github/mangelt/ms/get/invoice/service/ComprobantesProxy.java
@@ -2,15 +2,20 @@ package com.github.mangelt.ms.get.invoice.service;
 
 import java.util.List;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.github.mangelt.ms.get.invoice.config.RibbonConfig;
 import com.github.mangelt.sat.services.model.Comprobante;
 
-@FeignClient(name = "ms-all-invoices", url = "localhost:8080")
+@FeignClient(name = "ms-all-invoices")
+@RibbonClient(name="ms-all-invoices", configuration = RibbonConfig.class)
 public interface ComprobantesProxy
 {
 
+	@LoadBalanced
 	@GetMapping("/comprobante")
 	List<Comprobante> getAll();
 
